de20771af8bad829befdc9c6131d8455470f299e
Rodrigo Riquelme <rodrigo.riquelme.e@outlook.com>
2021-02-26 08:49:44 -0300

    remove bulma react components lib

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import './App.scss';
 import Footer from './containers/footer/Footer';
 import Navbar from './containers/navbar/Navbar';
 import Landing from './containers/landing/Landing';
-import LoginModal from "./components/login-modal/LoginModal";
 import ContextApi from "./context-api/ContextApi";
 import CookiesPolicies from "./components/cookies-policies/CookiesPolicies";
-import 'react-bulma-components/dist/react-bulma-components.min.css';
 
 function App() {
     return (
